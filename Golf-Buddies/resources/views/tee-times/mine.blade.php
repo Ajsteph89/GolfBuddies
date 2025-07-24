e9fb53ad64cb17d3a8526c1f22c2d1ed58e074b5
Ajsteph89 <ajsteph89@gmail.com>
2025-07-24 13:19:15 -0400

    updated create tee time button on my tee times to direct the user to courses page

--- Golf-Buddies/resources/views/tee-times/mine.blade.php
+++ Golf-Buddies/resources/views/tee-times/mine.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-between items-center mb-6">
                 <h1 class="text-2xl font-bold text-[#006341]">Upcoming Tee Times</h1>
-                <a href="{{ route('tee-times.create') }}" class="bg-green-600 text-white px-4 py-2 rounded">
+                <a href="{{ route('courses.index') }}" class="bg-green-600 text-white px-4 py-2 rounded">
                     + Create Tee Time
                 </a>
             </div>
